e240da21c842b1bd599a192204004213d58ad314
Umutcan <umutcangolbasi@gmail.com>
2024-10-08 18:40:39 +0300

    Testing yellow buttons in Button.tsx

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -10,15 +10,15 @@ const button = cva(
     "rounded-xl",
     "text-center",
     "border",
-    "border-green-400",
+    "border-yellow-400",
     "transition-colors",
     "delay-50",
   ],
   {
     variants: {
       intent: {
-        primary: ["bg-green-400", "text-white", "hover:enabled:bg-green-700"],
-        secondary: ["bg-transparent", "text-green-400", "hover:enabled:bg-green-400", "hover:enabled:text-white"],
+        primary: ["bg-yellow-400", "text-white", "hover:enabled:bg-yellow-700"],
+        secondary: ["bg-transparent", "text-yellow-400", "hover:enabled:bg-yellow-400", "hover:enabled:text-white"],
       },
       size: {
         sm: ["min-w-20", "h-full", "min-h-10", "text-sm", "py-1.5", "px-4"],
